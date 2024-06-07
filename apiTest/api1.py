1ed30a9eea30386b699227bcd4e0aa6593134910
kantgaurav <shashikant.gaurav@yahoo.com>
2024-06-07 18:09:30 +0530

    Added API TEST CASES

---
+++ apiTest/api1.py
@@ -0,0 +1,13 @@
+import requests as req
+
+resp=req.get("https://reqres.in/api/users?page=2/id=10")
+
+print(resp.json())
+
+status_code = resp.status_code
+print(status_code)
+assert status_code==200, "Wrong Response Code"
+
+#print(resp)
+#print(type(resp))
+#print(dir(resp))
--- gajanan.py
+++ gajanan.py
@@ -1,19 +1,24 @@
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 
-@pytest.fixture
-def driver():
+@pytest.fixture()
+def driver_setUp():
+    global driver
     driver = webdriver.Chrome()
-    yield driver
+    driver.implicitly_wait(10)
+    yield
     driver.quit()
 
 
-def test_svg_demo(driver):
+def test_flipkart_demo(driver_setUp):
+   
     driver.get("https://www.flipkart.com/")
+    driver.implicitly_wait(5)
     driver.maximize_window()
     search_input = driver.find_element(By.NAME, 'q')
     search_input.send_keys("AC")
@@ -21,6 +26,8 @@ def test_svg_demo(driver):
                                          "//*[local-name()='svg']/*[local-name()='g' and @fill-rule='evenodd']")
     actions = ActionChains(driver)
     actions.move_to_element(search_element).click().perform()
+ 
+def test_svg_map(driver_setUp):
     driver.get("https://www.amcharts.com/svg-maps/?map=india")
     states_list = driver.find_elements(By.XPATH,
                                        "//*[name()='svg']/*[name()='g'][7]/*[name()='g']/*[name()='g']/*[name() = 'path']")
