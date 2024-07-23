072b905e33b40b3c321c22fd3c1960e8b36a0f7f
imskgaurav <shashikant.gaurav@gmail.com>
2024-07-23 20:24:18 +0530

    Added 2 new Python PROGRAMS

---
+++ PythonPrograms/SwapTwoNumber.py
@@ -0,0 +1,7 @@
+x=7
+y=11
+x=x+y
+y=x-y
+x=x-y
+print(x)
+print(y)
---
+++ PythonPrograms/Table.py
@@ -0,0 +1,8 @@
+i = int (input('pls enter number'))
+print(i)
+
+for i in range(1,11):
+    for j in range(1, 11):
+         print(i*j, '\t', end ="")
+    print('\n')          
+         
